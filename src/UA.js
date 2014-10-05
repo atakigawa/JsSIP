8a921acdd454e2d645f454786e05b097c53d081f
Akinori Takigawa <akinori.takigawa@gmail.com>
2014-10-05 15:59:34 +0900

    use the specified uri schema rather than a "sip" literal (although jssip does not support sips so far.)

--- src/UA.js
+++ src/UA.js
@@ -908,7 +908,7 @@ UA.prototype.loadConfig = function(configuration) {
   this.contact = {
     pub_gruu: null,
     temp_gruu: null,
-    uri: new JsSIP.URI('sip', JsSIP.Utils.createRandomToken(8), settings.via_host, null, {transport: 'ws'}),
+    uri: new JsSIP.URI(settings.uri.scheme, JsSIP.Utils.createRandomToken(8), settings.via_host, null, {transport: 'ws'}),
     toString: function(options){
       options = options || {};
 
